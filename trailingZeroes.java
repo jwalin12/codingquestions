4d089177628f5ee01ecb5cb3be72b72ec8a9c6f4
jwalin12 <46630653+jwalin12@users.noreply.github.com>
2020-08-22 12:50:16 -0700

    Create trailingZeroes.java

---
+++ trailingZeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int trailingZeroes(int n) {
+      
+        if (n>0) {
+            return trailingZeroes(n/5) +n/5;
+        }
+        else {
+            return 0;
+        }
+       
+        
+        
+        
+    }
+    
+
+}
